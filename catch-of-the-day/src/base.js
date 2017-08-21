8d4203f507621fd2531fae77305352efcef432b8
Hector Palomares <hector.palomares@laureate.net>
2017-08-21 17:49:24 -0500

    Match state with Firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyBEDNo_TzNhkRMavTklekOfcbkTwE7auEw",
+  authDomain: "catch-of-the-day-66cae.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-66cae.firebaseio.com"
+});
+
+export default base;
